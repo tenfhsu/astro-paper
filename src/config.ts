c45fec3855ccd9cb76872fc37ce30349db0f36d4
tenfhsu <tenf@vip.qq.com>
2025-07-07 16:23:42 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -18,6 +18,6 @@ export const SITE = {
   },
   dynamicOgImage: true,
   dir: "ltr", // "rtl" | "auto"
-  lang: "en", // html lang code. Set this empty and default will be "en"
+  lang: "zh-CN", // html lang code. Set this empty and default will be "en"
   timezone: "Asia/Bangkok", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
 } as const;
